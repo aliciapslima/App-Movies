abae227e4b015601b5a5fdb5c2c4bfe09701a87b
lycie03 <lycieofficial@gmail.com>
2024-04-22 12:44:42 -0300

    add: header e footer

--- movies/src/App.js
+++ movies/src/App.js
@@ -1,8 +1,13 @@
+import Footer from "./components/Footer";
+import Header from "./components/Header";
+
 function App() {
   return (
     <>
+      <Header />
       <h1>Hello World!</h1>
       <p>Welcome to the app.</p>
+      <Footer />
     </>
     );
 }
